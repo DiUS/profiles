88eeafe21efe72864c9b79d0ecf526cf6c5541ac
azzamallow <azzamallow@gmail.com>
2013-10-25 16:26:03 +1100

    editable juice

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -6,34 +6,19 @@ angular.module('profilesApp')
     $scope.attrs.title = 'Senior Consultant, Cloud Lead';
     $scope.attrs.name = 'Charles Blaxland';
     $scope.attrs.labels = ['Java', 'Ruby', 'C++', 'Big Data', 'Mathematics', 'AWS', 'Agile and Lean', 'TDD', 'BDD'];
-    $scope.attrs.highlights = ['Led the ResMed Easycare Online system architecture and provided technical development leadership for the highly successful product. Also accountable for the work done across multiple teams consisting of a total of more than 60 people which consisted of DiUS, ResMed and other third party vendors.', 'Part of the DiUS team that was awarded the global ResMed Most Valuable Supplier Innovation Award.'];
-
-    $scope.enabledEditTitle = function () {
-      $scope.attrs.editingTitle = true;
-    };
-
-    $scope.disableEditTitle = function () {
-      $scope.attrs.editingTitle = false;
-    };
-
-    $scope.enabledEditName = function () {
-      $scope.attrs.editingName = true;
-    };
-
-    $scope.disableEditName = function () {
-      $scope.attrs.editingName = false;
-    };
+    $scope.attrs.highlights = [
+      {text: 'Led the ResMed Easycare Online system architecture and provided technical development leadership for the highly successful product. Also accountable for the work done across multiple teams consisting of a total of more than 60 people which consisted of DiUS, ResMed and other third party vendors.'}, 
+      {text: 'Part of the DiUS team that was awarded the global ResMed Most Valuable Supplier Innovation Award.'}
+    ];
 
     $scope.addExperience = function() {
       $scope.attrs.addingExperience = true;
     };
 
     $scope.addNewExperience = function(newExperience) {
-      $scope.attrs.highlights.push(newExperience);
+      $scope.attrs.highlights.push({text: newExperience});
       $scope.attrs.addingExperience = false;  
     };
 
-    $scope.disableEditTitle();
-    $scope.disableEditName();
     $scope.attrs.addingExperience = false;
   });
---
+++ app/scripts/directives/editable.js
@@ -0,0 +1,73 @@
+angular.module('profilesApp').directive( 'editInPlace', function() {
+  return {
+    restrict: 'A',
+    scope: { value:"=editInPlace" },    
+    template: '<span ng-click="edit()" ng-bind="value" ng-hide="editing"></span><input ng-show="editing" ng-model="value" ng-blur="editing = false" ng-enter="editing = false"></input>',
+    link: function ( $scope, element, attrs ) {
+      // Let's get a reference to the input element, as we'll want to reference it.
+      var inputElement = angular.element( element.children()[1] );
+      
+      // This directive should have a set class so we can style it.
+      element.addClass( 'edit-in-place' );
+      
+      // Initially, we're not editing.
+      $scope.editing = false;
+      
+      // ng-click handler to activate edit-in-place
+      $scope.edit = function () {
+        $scope.editing = true;
+        
+        // We control display through a class on the directive itself. See the CSS.
+        element.addClass( 'active' );
+        
+        // And we must focus the element. 
+        // `angular.element()` provides a chainable array, like jQuery so to access a native DOM function, 
+        // we have to reference the first element in the array.
+        inputElement[0].focus();
+      };
+      
+      // When we leave the input, we're done editing.
+      inputElement.prop( 'onblur', function() {
+        $scope.editing = false;
+        element.removeClass( 'active' );
+      });
+    }
+  };
+});
+
+angular.module('profilesApp').directive( 'editInPlaceTextarea', function() {
+  return {
+    restrict: 'A',
+    scope: { value:"=editInPlaceTextarea" },    
+    template: '<span ng-click="edit()" ng-bind="value" ng-hide="editing"></span><textarea ng-show="editing" ng-model="value" ng-blur="editing = false" ng-enter="editing = false"></textarea>',
+    link: function ( $scope, element, attrs ) {
+      // Let's get a reference to the input element, as we'll want to reference it.
+      var inputElement = angular.element( element.children()[1] );
+      
+      // This directive should have a set class so we can style it.
+      element.addClass( 'edit-in-place' );
+      
+      // Initially, we're not editing.
+      $scope.editing = false;
+      
+      // ng-click handler to activate edit-in-place
+      $scope.edit = function () {
+        $scope.editing = true;
+        
+        // We control display through a class on the directive itself. See the CSS.
+        element.addClass( 'active' );
+        
+        // And we must focus the element. 
+        // `angular.element()` provides a chainable array, like jQuery so to access a native DOM function, 
+        // we have to reference the first element in the array.
+        inputElement[0].focus();
+      };
+      
+      // When we leave the input, we're done editing.
+      inputElement.prop( 'onblur', function() {
+        $scope.editing = false;
+        element.removeClass( 'active' );
+      });
+    }
+  };
+});
