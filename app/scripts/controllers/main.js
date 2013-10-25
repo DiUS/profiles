b1ae6949b37d205cb4842c0ea94ae0ace524042f
azzamallow <azzamallow@gmail.com>
2013-10-25 13:55:48 +1100

    editing fields, border around profile

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,9 +2,26 @@
 
 angular.module('profilesApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    $scope.attrs = {};
+    $scope.attrs.title = 'Senior Consultant, Cloud Lead';
+    $scope.attrs.name = 'Charles Blaxland';
+
+    $scope.enabledEditTitle = function () {
+      $scope.attrs.editingTitle = true;
+    };
+
+    $scope.disableEditTitle = function () {
+      $scope.attrs.editingTitle = false;
+    };
+
+    $scope.enabledEditName = function () {
+      $scope.attrs.editingName = true;
+    };
+
+    $scope.disableEditName = function () {
+      $scope.attrs.editingName = false;
+    };
+
+    $scope.disableEditTitle();
+    $scope.disableEditName();
   });
---
+++ app/scripts/directives/ng-blur.js
@@ -0,0 +1,10 @@
+angular.module('profilesApp').directive('ngBlur', ['$parse', function($parse) {
+  return function(scope, element, attr) {
+    var fn = $parse(attr['ngBlur']);
+    element.bind('blur', function(event) {
+      scope.$apply(function() {
+        fn(scope, {$event:event});
+      });
+    });
+  }
+}])
