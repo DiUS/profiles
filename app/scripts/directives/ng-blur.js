3dd738abc4f7274724edfa422ffc50271a2e9f7c
azzamallow <azzamallow@gmail.com>
2013-10-25 15:11:58 +1100

    add a highlight

--- app/scripts/directives/ng-blur.js
+++ app/scripts/directives/ng-blur.js
@@ -8,3 +8,17 @@ angular.module('profilesApp').directive('ngBlur', ['$parse', function($parse) {
     });
   }
 }])
+
+angular.module('profilesApp').directive('ngEnter', function () {
+    return function (scope, element, attrs) {
+        element.bind("keydown keypress", function (event) {
+            if(event.which === 13) {
+                scope.$apply(function (){
+                    scope.$eval(attrs.ngEnter);
+                });
+
+                event.preventDefault();
+            }
+        });
+    };
+});
