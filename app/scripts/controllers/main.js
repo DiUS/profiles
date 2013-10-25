19fdd706be21e676323322337d622c7335f101e5
azzamallow <azzamallow@gmail.com>
2013-10-25 16:33:58 +1100

    added experience

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,6 +10,11 @@ angular.module('profilesApp')
       {text: 'Led the ResMed Easycare Online system architecture and provided technical development leadership for the highly successful product. Also accountable for the work done across multiple teams consisting of a total of more than 60 people which consisted of DiUS, ResMed and other third party vendors.'}, 
       {text: 'Part of the DiUS team that was awarded the global ResMed Most Valuable Supplier Innovation Award.'}
     ];
+    $scope.attrs.experiences = [
+      {text: 'Sensis'},
+      {text: 'Real Estate Australia'},
+      {text: 'Westfield'}
+    ];
 
     $scope.addExperience = function() {
       $scope.attrs.addingExperience = true;
